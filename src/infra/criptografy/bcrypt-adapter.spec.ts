40a19bd7c23ebccffe7ba33bd3fe8ab1968a8e2f
Cardoso <cardosoassuncao@gmail.com>
2022-04-15 14:56:00 -0400

    test: ensure BcryptAdapter throws if bcrypt throws

--- src/infra/criptografy/bcrypt-adapter.spec.ts
+++ src/infra/criptografy/bcrypt-adapter.spec.ts
@@ -1,13 +1,11 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt-adapter'
 
-jest.mock('bcrypt', () => {
-  return {
-    async hash(): Promise<string> {
-      return new Promise((resolve) => resolve('hash'))
-    }
+jest.mock('bcrypt', () => ({
+  async hash(): Promise<string> {
+    return new Promise((resolve) => resolve('hash'))
   }
-})
+}))
 
 const salt = 12
 const makeSut = (): BcryptAdapter => {
@@ -25,5 +23,13 @@ describe('Bcrypt Adapter', () => {
     const sut = makeSut()
     const hash = await sut.encrypt('any_value')
     expect(hash).toBe('hash')
+  })
+  test('should throw if bcrypt throws', async () => {
+    const sut = makeSut()
+    jest.spyOn(bcrypt, 'hash').mockImplementationOnce(async () => {
+      return new Promise((_resolve, reject) => reject(new Error()))
+    })
+    const promise = sut.encrypt('any_value')
+    await expect(promise).rejects.toThrow()
   })
 })
