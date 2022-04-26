d020ba98e2caa516d2d48bf822a5b8f76deaa710
Cardoso <cardosoassuncao@gmail.com>
2022-04-26 09:00:32 -0400

    refactor: update mongo-helper

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -2,19 +2,22 @@ import { Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient,
-  uri: null as string,
 
   async connect(uri: string): Promise<void> {
-    this.uri = uri
-    this.client = await MongoClient.connect(uri)
+    this.client = await MongoClient.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
   },
+
   async disconnect(): Promise<void> {
     await this.client.close()
-    this.client = null
   },
+
   getCollection(name: string): Collection {
     return this.client.db().collection(name)
   },
+
   map: (collection: any): any => {
     const { _id, ...accountWithoutId } = collection
     return { ...accountWithoutId, id: _id }
